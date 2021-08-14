bc7ee9a2a7d7845a994e3456e9024bd728045bf8
SLAST1 <64786416+SLAST1@users.noreply.github.com>
2021-08-14 04:19:56 +0300

    Add files via upload

---
+++ Weak3/Exercises/Reversible_String.cpp
@@ -0,0 +1,92 @@
+//Реализуйте класс ReversibleString, хранящий строку и 
+//поддерживающий методы Reverse для переворота строки и ToString для получения строки.
+//Пример:
+//Код
+int main() {
+  ReversibleString s("live");
+  s.Reverse();
+  cout << s.ToString() << endl;
+  
+  s.Reverse();
+  const ReversibleString& s_ref = s;
+  string tmp = s_ref.ToString();
+  cout << tmp << endl;
+  
+  ReversibleString empty;
+  cout << '"' << empty.ToString() << '"' << endl;
+  
+  return 0;
+}
+//Вывод
+evil
+live
+""
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class ReversibleString
+{
+private:
+    string name;
+ 
+public:
+    ReversibleString(){}
+    ReversibleString(const string& s){
+         name = s;
+    }
+
+    void Reverse(){
+        reverse(begin(name), end(name));
+    }
+
+    string ToString() const{
+        return name;
+    }
+};
+ 
+int main(){
+    ReversibleString s("live");
+    s.Reverse();
+    cout << s.ToString() << endl;
+ 
+    s.Reverse();
+    const ReversibleString& s_ref = s;
+    string tmp = s_ref.ToString();
+    cout << tmp << endl;
+ 
+    ReversibleString empty;
+    cout << '"' << empty.ToString() << '"' << endl;
+    return 0;
+}
+
+
+
+
+
+
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class ReversibleString{
+public:
+  ReversibleString(){}
+  ReversibleString(const string& s){
+    data = s;
+  }
+
+  void Reverse(){
+    reverse(begin(data), end(data));
+  }
+
+  string ToString() const{
+    return data;
+  }
+private:
+  string data;
+};
